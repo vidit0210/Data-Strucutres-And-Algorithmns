15f6823c4cab3728c64c1baed7d5b17c71bb29c5
Vidit Shah <vidit0210@gmail.com>
2018-12-19 10:23:04 +0530

    CharCount

--- ProbelmSolving/CharCount.js
+++ ProbelmSolving/CharCount.js
@@ -2,14 +2,15 @@ function CharCount(str) {
   let result = {};
   for (let i = 0; i < str.length; i++) {
     let char = str[i].toLowerCase();
-    if (result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
+    if (/[a-z0-9]/.test(char)) {
+      if (result[char] > 0) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
     }
   }
   return result;
 }
-let { i } = CharCount("Vidit");
-console.log(i);
-console.log(CharCount("Vidit"));
+
+console.log(CharCount("Vidit Shah !!!"));
