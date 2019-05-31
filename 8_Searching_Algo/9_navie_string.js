6ff3e335e3bf65d5a1760830284e03e5fd094f75
Vidit Shah <vidit0210@gmail.com>
2019-05-31 10:38:16 +0530

    Nv2

--- 8_Searching_Algo/9_navie_string.js
+++ 8_Searching_Algo/9_navie_string.js
@@ -1,1 +1,12 @@
 //Number of time a smaller strings upload in a longer string
+/*
+define a Function with longer string and lower string
+Loop over the Longer String
+Loop over the shorter string
+if the Characters don't match break our the inner loop
+if characters do match,keep going
+if you complete the inner loop nd find a match,increment the count of matches
+return the count
+ */
+
+//
