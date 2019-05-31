601e0f8f85efd2d6dfd250c444f6224190ac3eec
Vidit Shah <vidit0210@gmail.com>
2019-05-31 12:48:05 +0530

    Sorting

--- 8_Searching_Algo/9_navie_string.js
+++ 8_Searching_Algo/9_navie_string.js
@@ -8,5 +8,15 @@ if characters do match,keep going
 if you complete the inner loop nd find a match,increment the count of matches
 return the count
  */
+function naiveString(str, pattern) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      if (str[i + j] !== pattern[j]) break;
+      if (j == pattern.length - 1) count++;
+    }
+  }
+  return count;
+}
 
-//
+console.log(naiveString("Lorielollol loled", "lol"));
