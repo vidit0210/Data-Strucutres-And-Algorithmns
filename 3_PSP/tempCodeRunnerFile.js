82321fb53a715ac0841cdaf414dcc18af6204719
vidit0210 <0210vidit@gmail.com>
2019-06-07 17:27:39 +0530

    BubbleSort

--- 3_PSP/tempCodeRunnerFile.js
+++ 3_PSP/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(maxSumArray([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
+console.log(maxSumArray([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
--- 9_sorting_Algo/1_Bubble.js
+++ 9_sorting_Algo/1_Bubble.js
@@ -1,4 +1,21 @@
 //A bubble Sort Algorithmn
 //
 
-function BubbleSort(arr) {}
+const swap = (arr, index1, index2) => {
+  return ([arr[index1], arr[index2]] = [``arr[index2], arr[index1]]);
+};
+function BubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        // let temp = arr[i];
+        // arr[i] = arr[j];
+        // arr[j] = temp;
+        swap(arr, i, j);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(BubbleSort([4, 2, 7, 1, 9, 6, 3]));
