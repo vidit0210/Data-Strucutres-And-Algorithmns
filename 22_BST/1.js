25441d94df44ad2e467cc8fffb3c5cfda8251280
vidit0210 <0210vidit@gmail.com>
2019-09-03 16:01:45 +0530

    find+insert

--- 22_BST/1.js
+++ 22_BST/1.js
@@ -11,22 +11,60 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  checkAndMoveLeft(node){
-    return node.left == null;
-  }
-  checkAndMoveRight(node){
-      return node.right == null;
-      
-  }
   insertNode(value) {
     let newNode = new Node(value);
-    if (this.root != null) {
-      this.root = newNode(value);
-      return;
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
     }
-    if(newNode.value > )
+    let currentNode = this.root;
+    while (true) {
+      if (value == currentNode.value) return undefined;
+      if (value <= currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return this;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right == null) {
+          currentNode.right = newNode;
+          return this;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  findNode(value) {
+    let found = false;
+    if (this.root === null) return false;
+    let currentNode = this.root;
+    while (currentNode && !found) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        found = true;
+        return found;
+      }
+    }
+    if (!found) {
+      return false;
+    }
   }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
+tree.insertNode(10);
+tree.insertNode(5);
+tree.insertNode(13);
+tree.insertNode(11);
+tree.insertNode(2);
+tree.insertNode(16);
+tree.insertNode(7);
+tree.insertNode(7);
+
+console.log(tree.findNode(7));
