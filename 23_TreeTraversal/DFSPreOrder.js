ec762b5f8d6d463fa66f0c9c538eba5bb735ae85
vidit0210 <0210vidit@gmail.com>
2019-09-05 17:21:44 +0530

    InOrder PostOrder

--- 23_TreeTraversal/DFSPreOrder.js
+++ 23_TreeTraversal/DFSPreOrder.js
@@ -90,6 +90,29 @@ class BinarySearchTree {
     traverse(current);
     return data;
   }
+  DFSPostOrder() {
+    let data = [];
+    let current = this.root;
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(current);
+    return data;
+  }
+
+  DFSInOrder() {
+    let data = [];
+    let current = this.root;
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(current);
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -99,5 +122,5 @@ tree.insert(15);
 tree.insert(3);
 tree.insert(8);
 tree.insert(20);
-let data = tree.DFSPreOrder();
+let data = tree.DFSInOrder();
 console.log(data);
